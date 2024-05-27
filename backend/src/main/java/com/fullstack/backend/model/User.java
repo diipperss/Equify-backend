5994392f0a66e9b53ca36cee49e95359a9e38897
diipperss <dibya6665@gmail.com>
2024-05-27 22:05:47 +0800

    post and get implemented

---
+++ backend/src/main/java/com/fullstack/backend/model/User.java
@@ -0,0 +1,58 @@
+package com.fullstack.backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/* Created by Arjun Gautam */
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String department;
+    private String email;
+    private String password;
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ backend/src/main/java/com/fullstack/backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.fullstack.backend.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.fullstack.backend.model.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
+
